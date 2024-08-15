cdf4f36f18f91e2a16d8b63778a528d0fb900aec
Rafi Ferdos <rafiferdos@gmail.com>
2024-08-15 18:57:35 +0600

    chore: Add MongoDB connection and database setup

--- index.js
+++ index.js
@@ -22,3 +22,27 @@ app.options("*", cors(corsOptions));
 app.use(express.json());
 
 // Connection URL
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vtlc5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // connect database
+        const prodify_db = client.db("prodify");
+        const product_collection = prodify_db.collection("products");
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
