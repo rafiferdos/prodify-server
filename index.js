ffe66611db9eef24f95f61a3c189dc8fcaa703dc
Rafi Ferdos <rafiferdos@gmail.com>
2024-08-17 19:33:52 +0600

    chore: Update CORS origin to include production URL

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 9000;
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://prodify-45241.web.app'],
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
